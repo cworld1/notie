a98e10b892e7e916eb594c499c040820c8c86614
CWorld <cworld0@qq.com>
2023-12-10 00:29:11 +0800

    feat(app): :sparkles: initial app module

---
+++ app/src/main/java/com/cworld/notie/MainActivity.java
@@ -0,0 +1,11 @@
+package com.cworld.notie;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}
