9246103e8163119289908eeafb48547654b5a3a0
CWorld <cworld0@qq.com>
2023-12-14 21:41:13 +0800

    feat(edit): :sparkles: add edit view for more component bottom bar and content

--- app/src/main/java/com/cworld/notie/EditActivity.java
+++ app/src/main/java/com/cworld/notie/EditActivity.java
@@ -1,14 +1,38 @@
 package com.cworld.notie;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.color.DynamicColors;
 
 public class EditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+
+        initTopAppBar(findViewById(R.id.topAppBar));
+        initBottomAppBar(findViewById(R.id.bottomAppBar));
+    }
+
+    private void initTopAppBar(@NonNull MaterialToolbar topAppBar) {
+        topAppBar.setNavigationOnClickListener(v -> finish());
+    }
+
+    private void initBottomAppBar(@NonNull BottomAppBar bottomAppBar) {
+        bottomAppBar.setNavigationOnClickListener(item -> {
+            int itemId = item.getId();
+            if (itemId == R.id.actionCommand) {
+                
+            }
+        });
     }
 }
