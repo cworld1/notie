8290134fa1ceff82c19560caf440d76025e57e80
CWorld <cworld0@qq.com>
2023-12-16 01:06:00 +0800

    test(settings): :art: update settings ui

--- app/src/main/java/com/cworld/notie/SettingsActivity.java
+++ app/src/main/java/com/cworld/notie/SettingsActivity.java
@@ -1,14 +1,27 @@
 package com.cworld.notie;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.color.DynamicColors;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+
+        initTopAppBar(findViewById(R.id.topAppBar));
+    }
+
+    private void initTopAppBar(MaterialToolbar topAppBar) {
+        topAppBar.setNavigationOnClickListener(v -> finish());
     }
 }
